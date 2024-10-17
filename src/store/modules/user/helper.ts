976fbe2acf8cd165ca1c1a20d63579a50cb96de8
liumingxing <594086597@qq.com>
2024-10-17 18:48:09 +0800

    调整图标与连接

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -18,9 +18,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Dooy/chatgpt-web-midjourney-proxy/main/src/assets/avatar.jpg',
+      avatar: 'https://oss.javastarboy.com/agi/github-projects-logo.png',
       name:  t('mjset.sysname'),//'AI绘图',
-      description: 'Star on <a href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" class="text-blue-500" target="_blank" >GitHub</a>',
+      description: 'Star on <a href="https://github.com/javastarboy/chatgpt-web-midjourney-proxy" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
